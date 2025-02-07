d47abde39e9cb0821443c900f6e8e3e79f29ab73
Christian Elmar Michael Wiesinger <C.WIESINGER@htlkrems.at>
2025-02-07 17:10:17 +0100

    Synching

---
+++ AndersTaxi/Communications/GlobalConfig.cs
@@ -0,0 +1,8 @@
+﻿namespace Communications;
+
+public static class GlobalConfig
+{
+    public static string HostName { get; set; } = "rabbitmq";
+    public static int GlobalLegth;
+    public static int GlobalWidth;
+}
--- AndersTaxi/LogService/Program.cs
+++ AndersTaxi/LogService/Program.cs
@@ -6,19 +6,26 @@ class Program
 {
     static async Task Main(string[] args)
     {
+        ManualResetEvent _quitEvent = new ManualResetEvent(false);
+        Console.CancelKeyPress += (sender, eArgs) =>
+        {
+            _quitEvent.Set();
+            eArgs.Cancel = true;
+        };
+
         //Log for now to Console
         var f = await CommunicationHandlerFactory.Initialize("rabbitmq");
         var consumerHandler = await f.CreateConsumerHandler("log_queue");
-        
-        consumerHandler.OnMessage += (sender, messageArgs) =>
-        {
-            Console.WriteLine($"Logged Message: {messageArgs.Message}");
-        };
-        
-        //Keep alive
-        while (true)
-        {
-            await Task.Delay(10000);
-        }
+
+        consumerHandler.OnMessage += Consume;
+
+
+        //keep alive
+        _quitEvent.WaitOne();
+    }
+    
+    public static void Consume(object sender, MessageArgs e)
+    {
+        Console.WriteLine($"Logged Message: {e.Message}");
     }
 }
